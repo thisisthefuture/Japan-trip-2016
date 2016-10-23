0fbc15c829b1e230f13710fd819e786391beeb5d
thisisthefuture <github@aliceluu.com>
2016-10-22 22:00:39 -0700

    Added basic map that reads geoJSOP of visited locations

---
+++ Cities_GeoJSONP.js
@@ -0,0 +1,32 @@
+eqfeed_callback({
+"type": "FeatureCollection",
+
+"features": [
+{ "type": "Feature", "properties": { "Name": "Tokyo", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 139.691706, 35.689488, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "Takamatsu", "Description": "Known for Udon" }, "geometry": { "type": "Point", "coordinates": [ 134.046574, 34.342788, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "Naoshima", "Description": "Naoshima (直島) is an island located in the Seto Inland Sea of Japan. It is renowned for its collection of contemporary art galleries and exhibits." }, "geometry": { "type": "Point", "coordinates": [ 133.995689, 34.459703, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "Teshima", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 134.072257799999988, 34.4814834, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "Matsuyama", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 132.765575, 33.839157, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "Hiroshima", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 132.455293, 34.385203, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "Osaka", "Description": "Known for street food" }, "geometry": { "type": "Point", "coordinates": [ 135.502165, 34.693738, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "Ishigaki", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 124.15558, 24.340661, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "Taketomi Island", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 124.0889373, 24.3222475, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "Iriomote-jima", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 123.8187977, 24.3300613, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "Takaichi District", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 135.808993399999963, 34.4501055, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "Kyoto", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ 135.768029, 35.011636, 0.0 ] } }
+,
+{ "type": "Feature", "properties": { "Name": "Hakone", "Description": "Known for their hotsprings" }, "geometry": { "type": "Point", "coordinates": [ 139.10694, 35.232355, 0.0 ] } }
+
+]
+});
